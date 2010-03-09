de190877a654da1a0fbd4895f4901054f886032e
Mika Tuupola <tuupola@appelsiini.net>
2010-03-09 16:16:37 +0200

    Initial commit to rack_funky_cache.

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'rack_funky_cache'
+
+class Test::Unit::TestCase
+end
